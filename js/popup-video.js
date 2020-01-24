89a69f6a7c53db245b3b0c07f55aded66119fe7b
Jimmytran16 <jimmytran1620@gmail.com>
2020-01-24 13:52:34 -0500

    portfolio update

--- js/popup-video.js
+++ js/popup-video.js
@@ -14,3 +14,7 @@ $('#videoLink3').magnificPopup({
   type:'inline',
   midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 })
+$('#videoLink4').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+})
